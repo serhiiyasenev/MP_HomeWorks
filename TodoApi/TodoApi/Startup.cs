a23df6d8c1406f87fad70f652c927e180084148b
Serhii-Yasenev <serhii_yasenev@epam.com>
2018-08-15 14:16:52 +0300

    add Swagger

--- TodoApi/TodoApi/Startup.cs
+++ TodoApi/TodoApi/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 using TodoApi.Filters;
 using TodoApi.Models;
 
@@ -19,21 +20,42 @@ namespace TodoApi
 
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddDbContext<TodoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TodoContext")));
             services.AddMvc(options =>
             {
                 options.Filters.Add(
-                    new AddHeaderAttribute("GlobalAddHeader", "Result filter added to MvcOptions.Filters")); // an instance
-                options.Filters.Add(typeof(ActionFilter)); // by type
+                    new AddHeaderAttribute("GlobalAddHeader", "Result filter added to MvcOptions.Filters"));
+                options.Filters.Add(typeof(ActionFilter));
             });
 
-            //services.AddDbContext<TodoContext>(options =>
-            //        options.UseSqlServer(Configuration.GetConnectionString("TodoContext")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "My API",
+                    Version = "v1",
+                    Contact = new Contact
+                    {
+                        Name = "Serhii Yasenev",
+                        Url = "https://github.com/serhiiyasenev"
+                    }
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // Specifying the Swagger JSON endpoint,
+            // Swagger JSON endpoint just as localhost:<port>
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseMvc();
         }
     }
