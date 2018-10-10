24c891336250f80f43b9495e47fe48363ac7b496
Serhii-Yasenev <serhii_yasenev@epam.com>
2018-10-10 20:26:25 +0300

    udpd

---
+++ SuperApp.Api/Controllers/DataController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string api)
+        {
+            // TODO: Add logic
+
+            return "value";
+        }
+
+    }
+}
